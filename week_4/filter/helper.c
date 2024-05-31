5c38c0f226c35032e768b141521c6d5b6dab1d84
Dat Nguyen <datnguyenftu210@gmail.com>
2024-05-31 14:09:10 +0700

    feat: problem set 4 - filter - grayscale

--- week_4/filter/helper.c
+++ week_4/filter/helper.c
@@ -5,7 +5,16 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            BYTE average = round((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3.0);
+            image[i][j].rgbtRed = average;
+            image[i][j].rgbtGreen = average;
+            image[i][j].rgbtBlue = average;
+        }
+    }
 }
 
 // Convert image to sepia
